8a31ee431d1c1c8414920a43fcb82f87f84232e3
StasikLeyshin <74827092+stasikleyshin@users.noreply.github.com>
2023-10-21 20:22:01 +0300

    adding an app launcher

--- cmd/userapi/main.go
+++ cmd/userapi/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
 	"log"
+	"refactoring/internal/app"
 	"refactoring/internal/app/startup"
+	"refactoring/internal/http"
 	"refactoring/internal/service"
 )
 
@@ -12,13 +15,16 @@ func main() {
 
 	//configPath := os.Getenv("CONFIG_PATH")
 
+	// Файл с конфигурацией проекта
 	configPath := "config/config.local.yaml"
 
+	// Парсим файл конфигурации
 	config, err := startup.NewConfig(configPath)
 	if err != nil {
 		log.Fatalf("failed to Config %v", err)
 	}
 
+	// Создаём логгер
 	logger := startup.NewLogger(loggerName)
 
 	// Клиент для реализации бизнес-логики
@@ -28,11 +34,9 @@ func main() {
 		log.Fatalf("failed Start Service: %v", err)
 	}
 
-	//// Запуск http сервера
-	//httpRouter := http.NewHttpRouter(config.Http, client)
-	//log.Printf("start http-server")
-	//err = httpRouter.Start()
-	//if err != nil {
-	//	log.Fatalf("failed to listen the port: %d %v", config.Http.Port, err)
-	//}
+	// Создаём экземпляр http сервера
+	httpRouter := http.NewHttpRouter(config.Http, client, logger)
+
+	// Запускаем http сервер
+	app.NewApp(logger, httpRouter).Run(context.Background())
 }
